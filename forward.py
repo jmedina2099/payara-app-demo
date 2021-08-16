f17c5e5cf6aae793bd666d399e4b8d1197268bcd
Jorge Alberto Medina Rosas <jarosas@stefanini.com>
2021-08-16 15:36:44 -0500

    more sleep

--- forward.py
+++ forward.py
@@ -1,3 +1,4 @@
 import os
 
+print('*********************** FORWARD PORT          *********************************')
 os.spawnl(os.P_DETACH,'C:\\Users\\jarosas\\Desktop\\kubectl\\kubectl.exe','kubectl', 'port-forward', 'service/demo-service', '8080:80')
--- sleep.py
+++ sleep.py
@@ -1,2 +1,2 @@
 import time
-time.sleep(75)
+time.sleep(200)
